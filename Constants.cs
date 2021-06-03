a6c7157a9fd8e7173ba4a2003a64849ebf659082
maksbohdanov <76075992+maksbohdanov@users.noreply.github.com>
2021-06-03 09:01:32 +0300

    Add files via upload

---
+++ Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FootballAPI
+{
+    public class Constants
+    {
+        public static string adress = "https://api-football-v1.p.rapidapi.com";
+        public static string apiKey = "xxxxxxxx";
+        public static string AccessKey = "xxxxxxxxxx";
+        public static string Secret = "xxxxxxx";
+        public static string TableName = "xxxxxxxxxxx";
+    }
+}

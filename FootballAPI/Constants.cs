7e4c47ddb6c5db70206ae03757e2fca882586d3d
maksbohdanov <76075992+maksbohdanov@users.noreply.github.com>
2021-11-22 22:24:04 +0200

    Update Constants.cs

--- FootballAPI/Constants.cs
+++ FootballAPI/Constants.cs
@@ -8,9 +8,9 @@ namespace FootballAPI
     public class Constants
     {
         public static string adress = "https://api-football-v1.p.rapidapi.com";
-        public static string apiKey = "9e5967798emshaa4531776853c38p182046jsn489b24d3df80";
-        public static string AccessKey = "AKIA43RWLICNNWPS2ZYY";
-        public static string Secret = "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";
+        public static string apiKey = "xxxxxxxxx";
+        public static string AccessKey = "xxxxxxxxxxx";
+        public static string Secret = "xxxxxxxxxxx";
         public static string TableName = "Fixtures";
     }
 }

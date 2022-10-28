3d79e700ad90fd4751be267e77febd5768f112d1
Maksym Bohdanov <maks.bohdanov27@gmail.com>
2022-10-28 12:13:32 +0300

    Implement controllers

--- FootballAPI/Controllers/FixtureController.cs
+++ FootballAPI/Controllers/FixtureController.cs
@@ -1,17 +1,10 @@
-﻿using FootballAPI.Abstractions;
-using FootballAPI.Clients;
-using FootballAPI.Models;
-using FootballAPI.Models.Data;
-using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using FootballAPI.Models.Responses;
-using Amazon.DynamoDBv2;
-using Amazon.DynamoDBv2.Model;
-using FootballAPI.Extensions;
 using Microsoft.AspNetCore.Http;
+using BLL.Interfaces;
+using BLL.Responses;
+using BLL.Extensions;
 
 namespace FootballAPI.Controllers
 {
@@ -19,106 +12,44 @@ namespace FootballAPI.Controllers
     [ApiController]
     public class FixtureController : ControllerBase
     {
-        private readonly IFixtureRepository _fixtureRepository;
-        private readonly IDynamoDBClient _dynamoDbClient;
+        private readonly IFixtureService _fixtureService;
 
 
-        public FixtureController(IFixtureRepository fixtureRepository, IDynamoDBClient dynamoDBClient)
+        public FixtureController(IFixtureService fixtureService)
         {
-            _fixtureRepository = fixtureRepository;
-            _dynamoDbClient = dynamoDBClient;
+            _fixtureService = fixtureService;
         }
 
         [HttpGet("teamUpcoming")]
         public async Task<List<FixtureResponse>> FixtureUpcomingByTeam([FromQuery] string team)
         {
-            var fixtures = await _fixtureRepository.GetFixtureUpcomingByTeam(team);
+            var fixtures = await _fixtureService.GetFixtureUpcomingByTeam(team);
             var result = new List<FixtureResponse>();
             if (fixtures != null)
             {
                 foreach (var item in fixtures.Response)
-                {
-                    var toAdd = new FixtureResponse
-                    {
-                        IdFixture = item.Fixture.Id,
-                        Date = item.Fixture?.Date.ToString(),
-                        Stadium = item.Fixture?.Venue?.Name ?? null,
-                        City = item.Fixture?.Venue?.City ?? null,
-                        Status = item.Fixture?.Status?.Long ?? null,
-                        League = new LeagueFixtureResponse
-                        {
-                            Id = item.League.Id,
-                            Name = item.League.Name,
-                            Country = item.League?.Country,
-                            Season = item.League?.Season,
-                        },
-                        Teams = new TeamFixtureResponse
-                        {
-                            IdHome = item.Teams.Home.Id,
-                            NameHome = item.Teams.Home.Name,
-                            GoalHome = item.Goals?.Home ?? null,
-
-                            IdAway = item.Teams.Away.Id,
-                            NameAway = item.Teams.Away.Name,
-                            GoalAway = item.Goals?.Away ?? null,
-
-                        },
-                        Odds = new OddsResponse
-                        {
-                            Home = item.Odds.FirstOrDefault()?.Response.FirstOrDefault().Bookmakers.FirstOrDefault().Bets.FirstOrDefault().Values[0].Odd ?? null,
-                            Draw = item.Odds.FirstOrDefault()?.Response.FirstOrDefault().Bookmakers.FirstOrDefault().Bets.FirstOrDefault().Values[1].Odd ?? null,
-                            Away = item.Odds.FirstOrDefault()?.Response.FirstOrDefault().Bookmakers.FirstOrDefault().Bets.FirstOrDefault().Values[2].Odd ?? null,
-                        },
-                    };
-                    result.Add(toAdd);
+                {                  
+                    result.Add(item.Convert());
                 }
                 return result;
             }
             else { return null; }
-
         }
 
         [HttpGet("teamLast")]
         public async Task<List<FixtureResponse>> FixtureLastByTeam([FromQuery] string team)
         {
-            var fixtures = await _fixtureRepository.GetFixtureLastByTeam(team);
+            var fixtures = await _fixtureService.GetFixtureLastByTeam(team);
             var result = new List<FixtureResponse>();
             if (fixtures != null)
             {
                 foreach (var item in fixtures.Response)
                 {
-                    var toAdd = new FixtureResponse
-                    {
-                        IdFixture = item.Fixture.Id,
-                        Date = item.Fixture?.Date.ToString(),
-                        Stadium = item.Fixture?.Venue?.Name,
-                        City = item.Fixture?.Venue?.City,
-                        Status = item.Fixture?.Status?.Long,
-                        League = new LeagueFixtureResponse
-                        {
-                            Id = item.League.Id,
-                            Name = item.League.Name,
-                            Country = item.League?.Country,
-                            Season = item.League?.Season,
-                        },
-                        Teams = new TeamFixtureResponse
-                        {
-                            IdHome = item.Teams.Home.Id,
-                            NameHome = item.Teams.Home.Name,
-                            GoalHome = item.Goals?.Home,
-
-                            IdAway = item.Teams.Away.Id,
-                            NameAway = item.Teams.Away.Name,
-                            GoalAway = item.Goals?.Away,
-
-                        },
-                    };
-                    result.Add(toAdd);
+                    result.Add(item.Convert());
                 }
                 return result;
             }
             else { return null; }
-
         }       
 
 
@@ -127,52 +58,11 @@ namespace FootballAPI.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetAll([FromQuery] string user)
         {
-            var response = await _dynamoDbClient.GetAll(user);
-
-            if (response == null)
+            var result = await _fixtureService.GetFavoriteFixtures(user);
+            if(result == null)
                 return NotFound("There are no records in db");
 
-            var result = response
-                .Select(result => new FixtureResponse()
-                {
-                    Id = result.Id,
-                    IdChat = result.IdChat,
-                    IdFixture = result.IdFixture,
-                    Date = result.Date ,
-                    Stadium = result.Stadium ,
-                    City = result.City ,
-                    Status = result.Status,
-                    League = new LeagueFixtureResponse
-                    {
-                        Id = result.IdLeague,
-                        Name = result.League,
-                        Country = result.Country    ,
-                        Season = result.Season
-                    },
-                    Teams = new TeamFixtureResponse
-                    {
-                        IdHome = result.IdHome,
-                        NameHome = result.NameHome,
-                        GoalHome = result.GoalHome !="no data" ? Int32.Parse(result.GoalHome) : null,
-
-
-                        IdAway = result.IdAway,
-                        NameAway = result.NameAway,
-                        GoalAway = result.GoalAway != "no data" ? Int32.Parse(result.GoalAway) : null
-                    },
-                    Odds = new OddsResponse
-                    {
-                        Home = result.Home,
-                        Draw = result.Draw,
-                        Away = result.Away
-
-                    }
-                })
-                .ToList();
-
             return Ok(result);
-
-
         }
 
         //POST
@@ -181,34 +71,7 @@ namespace FootballAPI.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> AddToFavorites([FromBody] FixtureResponse fixture)
         {
-            var data = new FixtureDBResponse
-            {
-                IdFixture = fixture.IdFixture,
-                IdChat = fixture.IdChat,
-                Date = fixture?.Date ?? null,
-                Stadium = fixture?.Stadium ?? null,
-                City = fixture?.City ?? null,
-                Status = fixture.Status,
-                IdLeague = fixture.League.Id,
-                League = fixture.League.Name,
-                Country = fixture?.League?.Country ?? null,
-                Season = fixture?.League?.Season ?? null,
-
-                Home = fixture?.Odds?.Home ?? null,
-                Draw = fixture?.Odds?.Draw ?? null,
-                Away = fixture?.Odds?.Away ?? null,
-
-                IdHome = fixture.Teams.IdHome,
-                NameHome = fixture.Teams.NameHome,
-                GoalHome = fixture.Teams.GoalHome != null ? fixture.Teams.GoalHome.ToString() : null,
-
-
-                IdAway = fixture.Teams.IdAway,
-                NameAway = fixture.Teams.NameAway,
-                GoalAway = fixture.Teams.GoalAway != null ? fixture.Teams.GoalAway.ToString() : null
-            };
-
-            var result = await _dynamoDbClient.PostFixture(data);
+            var result = await _fixtureService.AddToFavorites(fixture);
 
             if (result == false)
             {
@@ -225,7 +88,7 @@ namespace FootballAPI.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> DeleteFromFavorites([FromQuery] string id)
         {
-            var result = await _dynamoDbClient.Delete(id);
+            var result = await _fixtureService.DeleteFromFavorites(id);
             if (result == false)
             {
                 return BadRequest("Cannot delete value from database. Please see console log");
@@ -233,8 +96,5 @@ namespace FootballAPI.Controllers
 
             return Ok("Value has been successfully deleted from DB");
         }
-
-
-
     }
 }
--- FootballAPI/Controllers/TeamStatisticsController.cs
+++ FootballAPI/Controllers/TeamStatisticsController.cs
@@ -3,10 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using FootballAPI.Abstractions;
-using FootballAPI.Models.Data;
-using FootballAPI.Models.Responses;
-
+using BLL.Interfaces;
+using BLL.Extensions;
+using BLL.Responses;
 
 namespace FootballAPI.Controllers
 {
@@ -14,56 +13,23 @@ namespace FootballAPI.Controllers
     [ApiController]
     public class TeamStatisticsController : ControllerBase
     {
-        private readonly ITeamStatisticsRepository _teamStatisticsRepository;
+        private readonly ITeamStatisticsService _teamStatisticsService;
 
-        public TeamStatisticsController(ITeamStatisticsRepository teamStatisticsRepository)
+        public TeamStatisticsController(ITeamStatisticsService teamStatisticsService)
         {
-            _teamStatisticsRepository = teamStatisticsRepository;
+            _teamStatisticsService = teamStatisticsService;
         }
 
 
         [HttpGet("statName")]
         public async Task<TeamStatisticsResponse> TeamStaticticsByName([FromQuery] string team, string league)
         {
-            var teamStat = await _teamStatisticsRepository.GetTeamStaticticsByName(team, league);
+            var teamStat = await _teamStatisticsService.GetTeamStaticticsByName(team, league);
             if (teamStat != null)
             {
-                var result = new TeamStatisticsResponse
-                {
-                    Id = teamStat.Response.Team.Id,
-                    Team = teamStat.Response.Team.Name,
-                    LeagueId = teamStat.Response.League.Id,
-                    League = teamStat.Response.League.Name,
-                    Season = teamStat.Response.League.Season,
-                    Games = new GamesTeamStatResp
-                    {
-                        Total = teamStat.Response.Fixtures.Played.Total,
-                        Wins = teamStat.Response.Fixtures.Wins.Total,
-                        Loses = teamStat.Response.Fixtures.Loses.Total,
-                        Draws = teamStat.Response.Fixtures.Draws.Total,
-                        CleanSheet = teamStat.Response.CleanSheet?.Total
-
-                    },
-                    Goals = new GoalsTeamStatResp
-                    {
-                        TotalFor = teamStat.Response.Goals.For.Total.Total,
-                        AverageFor = teamStat.Response.Goals.For.Average.Total,
-                        TotalAgainst = teamStat.Response.Goals.Against.Total.Total,
-                        AverageAgainst = teamStat.Response.Goals.Against.Average.Total,
-                    },
-                    Penalties = new PenaltyTeamStatResp
-                    {
-                        Total = teamStat.Response.Penalty?.Total,
-                        Scored = teamStat.Response.Penalty?.Scored.Total,
-                        Missed = teamStat.Response.Penalty?.Missed.TotalResult
-                    }
-                };
-
-                return result;
+                return teamStat.Convert();
             }
-            else { return null; }
-
-            
+            else { return null; }            
         }
 
 
@@ -72,42 +38,11 @@ namespace FootballAPI.Controllers
         {
             try
             {
-                var leagueStand = await _teamStatisticsRepository.GetLeagueStandingsByName(league, country);
+                var leagueStand = await _teamStatisticsService.GetLeagueStandingsByName(league, country);
                 var statList = new List<Stat>();
                 if (leagueStand != null)
                 {
-                    foreach (var item in leagueStand.Response.FirstOrDefault().League.Standings.FirstOrDefault())
-                    {
-                        var toAdd = new Stat
-                        {
-                            Id = item.Team.Id,
-                            Team = item.Team.Name,
-                            Rank = item.Rank,
-                            Points = item.Points,
-                            Played = item.All.Played,
-                            Win = item.All.Win,
-                            Draw = item.All.Draw,
-                            Lose = item.All.Lose,
-                            GoalsFor = item.All.Goals.For,
-                            GoalsAgainst = item.All.Goals.Against,
-                            GoalsDiff = item.GoalsDiff
-                        };
-                        statList.Add(toAdd);
-                    }
-
-                    if (leagueStand != null)
-                    {
-                        var result = new StandingsResponse
-                        {
-                            League = leagueStand.Response.FirstOrDefault().League.Name,
-                            Country = leagueStand.Response.FirstOrDefault().League.Country,
-                            Season = leagueStand.Response.FirstOrDefault().League.Season,
-                            TeamsStat = statList
-                        };
-
-                        return result;
-                    }
-                    else { return null; }
+                    return leagueStand.Convert();   
                 }
                 else { return null; }
                 
@@ -121,13 +56,13 @@ namespace FootballAPI.Controllers
 
 
         [HttpGet("Teaminfo")]
-        public async Task<TeamInfoDBResponse> TeamInfo([FromQuery] string name)
+        public async Task<TeamInfoResponse> TeamInfo([FromQuery] string name)
         {
-            var team = await _teamStatisticsRepository.GetTeamInfo(name);
+            var team = await _teamStatisticsService.GetTeamInfo(name);
 
             if (team != null)
             {
-                var result = new TeamInfoDBResponse
+                var result = new TeamInfoResponse
                 {
                     Id = team.Response.FirstOrDefault().Team.Id,
                     Name = team.Response.FirstOrDefault().Team.Name,
@@ -135,9 +70,7 @@ namespace FootballAPI.Controllers
                 };
                 return result;
             }
-            else { return null; }
-            
-        }
-       
+            else { return null; }            
+        }       
     }
 }
